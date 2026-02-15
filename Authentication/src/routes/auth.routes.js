c6d1e67c39fa6a3bb50e8f68fa697b2ef0d15d65
rahulprajap12 <rahulprajapati1234123@gmail.com>
2026-02-15 11:32:51 +0530

    today i am create JWT tocaken

--- Authentication/src/routes/auth.routes.js
+++ Authentication/src/routes/auth.routes.js
@@ -1,35 +1,89 @@
 import express from 'express'
 import userModel from '../models/user.model.js';
+import jwt from 'jsonwebtoken';
 
-const router =express.Router();
+const router = express.Router();
 
 // router.use(express.json())
 
-router.post('/register' ,async(req,res)=>{
-    const {username,password}=req.body;
+router.post('/register', async (req, res) => {
+    const { username, password } = req.body;
 
-    const user=await userModel.create({
-        username,password
-    })
+    const user = await userModel.create({
+        username,
+        password
+    });
+
+    const token = jwt.sign(
+        { id: user._id },
+        process.env.JWT_SECRET
+    );
+
     res.status(201).json({
-        message:"user registerd successfully",
-        user
+        message: "user registered successfully",
+        user,
+        token
+    });
+});
+
+
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    const isUserExists = await userModel.findOne({
+        username: username
     })
-})
 
-router.post('/login',async(req,res)=>{
-    const {username,password}=req.body;
 
-    const isUserExists=await userModel.findOne({
-        username:username
-    })
 
-    if(!isUserExists){
+    if (!isUserExists) {
         return res.status(401).json({
-            message:"user account not found [invalid username]"
+            message: "user account not found [invalid username]"
         })
-        
     }
+    const isPasswordValid = password == isUserExists.password
+    if (!isPasswordValid) {
+        return res.status(401).json({
+            message: "nvalid password"
+        })
+    }
+    res.status(200).json({
+        message: "user loggend successfully"
+    })
+
 })
+
+router.get('/user', async (req, res) => {
+    const { token } = req.body
+
+    if (!token) {
+        return res.status(401).json({
+            message: "Unauthorized"
+        })
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        const user=await userModel.findOne({
+            _id:decoded.id  
+      })
+      res.status(200).json({
+        message:"user data fetched successfully",
+        user
+      })
+
+    }catch(err){
+        return res.status(401).json({
+            message:"Unauthorized - Invalid token"
+        })
+    }
+
+    // console.log(token);
+
+
+})
+
+
+
 
 export default router;
